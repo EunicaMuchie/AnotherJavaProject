46a48f8000a6f52d87085f41b4a77b579d185401
EunicaMuchie <89991738+EunicaMuchie@users.noreply.github.com>
2023-08-18 11:38:06 +0200

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,55 @@
+public class Main {
+    public static void main(String[] args) {
+
+        Contact.contacts.add(new Contact("Tinashe ","12355555667"));
+        Contact.contacts.add(new Contact("Tinashe ","12355555667"));
+        Contact.contacts.add(new Contact("Tinashe ","12355555667"));
+        Contact.contacts.add(new Contact("Tinashe ","12355555667"));
+        Contact.contacts.add(new Contact("Tinashe ","12355555667"));
+        Contact.contacts.add(new Contact("Tinashe ","12355555667"));
+        System.out.println("....... Welcome to my Contact Manager");
+        System.out.println("You have: " + Contact.contacts.size() + " contacts in your list.");
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//        System.out.println(" hello world");
+//
+//        double salary = 1.456;
+//        int intSalary = (int)salary;
+//        System.out.println(salary);
+//        System.out.println(intSalary);
+//
+//        int a = 12;
+//        int b = 89;
+//
+//        if (a > b){
+//            System.out.println("a is bigger");
+//        } else if (a < b) {
+//            System.out.println("a is smaller");
+//        }
+//
+//        boolean isTrue = (a < b);
+//        System.out.println(isTrue);
+//
+//        boolean isFalse = (a > b);
+//        System.out.println(isFalse);
+//
+//        boolean isTruee = (a != b);
+//        System.out.println(isTruee);
+//
+//        boolean isFalsee = (a == b);
+//        System.out.println(isFalsee);
+    }
+}
